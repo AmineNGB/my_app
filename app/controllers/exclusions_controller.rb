c6c5e78a60313f2b055dadcd912627a8b8b177b6
Amine NEGHBEL <amine.neghbel@enercoop.org>
2025-03-16 12:15:10 +0100

    dd

--- app/controllers/exclusions_controller.rb
+++ app/controllers/exclusions_controller.rb
@@ -13,22 +13,44 @@ class ExclusionsController < ApplicationController
 
   def update
     max_exclusions = User.joins(:group_memberships)
-                 .where(group_memberships: { group_id: current_user.group_ids }).where.not(id: current_user.id)
-                 .where(gender: opposite_gender(current_user.gender)).distinct.count # Define your max limit
+                  .where(group_memberships: { group_id: current_user.group_ids })
+                  .where.not(id: current_user.id)
+                  .where(gender: opposite_gender(current_user.gender)).distinct.count
+
+    # Vérifier avant d'appliquer la requête SQL
+    if (params[:exclusion_ids] || []).count > max_exclusions
+      # flash.now[:alert] = "Vous ne pouvez exclure que #{max_exclusions} utilisateurs au maximum."
+
+      # Réinitialisation des variables pour éviter un bug avec `render :edit`
+      @users = User.joins(:group_memberships)
+                  .where(group_memberships: { group_id: current_user.group_ids })
+                  .where.not(id: current_user.id)
+                  .order(:id).distinct
+      @exclusion_ids = current_user.excluded_users.pluck(:id)
+
+      flash[:alert] = "Vous ne pouvez exclure que #{max_exclusions} utilisateurs au maximum."
+      return redirect_to edit_exclusions_path
+    end
 
     new_excluded_users = User.where(id: params[:exclusion_ids] || []).limit(max_exclusions)
-    if new_excluded_users.count > max_exclusions
-      flash[:alert] = "Vous ne pouvez exclure que #{max_exclusions} utilisateurs au maximum."
-      return render :edit
-    end
-    # Mise à jour des exclusions avec `update`
+
     if current_user.update(excluded_users: new_excluded_users)
       redirect_to root_path, notice: "Liste mise à jour avec succès !"
     else
       flash[:alert] = "Erreur lors de la mise à jour."
+
+      # Recharger les variables avant render
+      @users = User.joins(:group_memberships)
+                  .where(group_memberships: { group_id: current_user.group_ids })
+                  .where.not(id: current_user.id)
+                  .order(:id).distinct
+      @exclusion_ids = current_user.excluded_users.pluck(:id)
+
       render :edit
     end
   end
+
+
 
   private
 
