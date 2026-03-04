5be5cd254424c74c8adc9f3dd9c25968a2427adf
Amine NEGHBEL <amine.neghbel@enercoop.org>
2026-03-04 22:43:04 +0100

    aze

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+         :recoverable, :rememberable,
+         :timeoutable
 
   has_one_attached :avatar
   has_many :exclusions, dependent: :destroy
